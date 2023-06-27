9d160248d8b08623c543f2c7bef14353756b9361
Barasa-Micah <barasamica@gmail.com>
2023-06-27 09:58:58 +0300

    Added elements in the javascript

---
+++ js/index.js
@@ -0,0 +1,4 @@
+//some of the elements we will use
+const search = document.getElementById('search')
+const userList = document.getElementById('user-list')
+const repoList = document.getElementById('repo-list')
